bb1e9b5aa507ba67b89653fc7cbb5395f1608700
JuliaSvn <s233004@dtu.dk>
2024-02-22 09:50:08 +0100

    Update Step2 with model
    Add the model (inch on a des bonnes valeurs, j'ai pas encore check)
    Change the availability (change for each windturbine)

--- Step2/step2.py
+++ Step2/step2.py
@@ -21,8 +21,8 @@ Csu = generationUnits_parameters['Csu'].values
 Uini = generationUnits_parameters['Uini'].values
 
 generation_units = GenerationUnits()
-nbUnits = generationUnits_parameters.shape[0] 
-for unit_id in range(nbUnits):
+nbUnitsConventionnal = generationUnits_parameters.shape[0] 
+for unit_id in range(nbUnitsConventionnal):
     generation_units.add_unit(
         unit_id=unit_id,
         node_id=nodes[unit_id],
@@ -43,17 +43,18 @@ pmax = wind_parameters['Pmax'].values
 pmin = wind_parameters['Pmin'].values
 costs = wind_parameters['Ci'].values
 
-scenario = "V1"
+scenario = "V1" #scenario : from V1 to V100
 
-nbUnits = wind_parameters.shape[0]
-for unit_id in range(nbUnits):
+nbUnitsWind = wind_parameters.shape[0]
+nbUnits = nbUnitsConventionnal + nbUnitsWind
+for unit_id in range(nbUnitsWind):
 
-    availability = pd.read_csv('inputs/data/scen_zone8.csv', sep=",")[scenario].values.tolist()
+    availability = pd.read_csv(f'inputs/data/scen_zoneW{unit_id}.csv', sep=",")[scenario].values.tolist()
 
     generation_units.add_unit(
         unit_id=unit_id,
         node_id=nodes[unit_id],
-        unit_type="wind turbine",
+        unit_type="wind_turbine",
         cost=costs[unit_id],
         pmax=pmax[unit_id],
         pmin=pmin[unit_id],
@@ -70,6 +71,7 @@ from loadUnits import LoadUnits
 
 #parameter load
 total_needed_demand = pd.read_csv('inputs/load_profile.csv', sep=';')['total_demand'].values
+nbHour = total_needed_demand.shape[0]
 
 load_location = pd.read_csv('inputs/load_location.csv', index_col='load_number', sep=';')
 nodes = load_location['node'].values
@@ -81,9 +83,54 @@ for unit_id in range(nbLoadUnits):
     load_units.add_unit(
         load_id=unit_id,
         node_id=nodes[unit_id],
-        bid_price=50,
+        bid_price=50,               
         load_percentage=load_percentage[unit_id],
         total_needed_demand=total_needed_demand
     )
 
 load_units.export_to_json()
+
+
+################################################################################
+# Model
+################################################################################
+# create a new model
+m = gp.Model()
+
+# Create variables 
+production = m.addMVar(shape=(nbHour, nbUnits), lb=0, name="power_generation", vtype=GRB.CONTINUOUS)
+demand_supplied = m.addMVar(shape=(nbHour, nbLoadUnits), lb=0, name=f"demand_supplied", vtype=GRB.CONTINUOUS) 
+
+# Set objective function
+
+objective = (gp.quicksum(demand_supplied[t, l]*load_units.units[l]['Bid price'] for t in range(nbHour) for l in range(nbLoadUnits)) 
+        - gp.quicksum(production[t, g]*generation_units.units[g]['Cost'] for t in range(nbHour) for g in range(nbUnits)))
+
+m.setObjective(objective, GRB.MAXIMIZE)
+
+# define constraints
+max_prod_constraint = [m.addConstr(production[t, g] <= generation_units.units[g]['PMAX']*generation_units.units[g]['Availability'][t]) for g in range(nbUnits) for t in range(nbHour)]
+demand_supplied_constraint = [m.addConstr(demand_supplied[t, l] <= load_units.units[l]['Needed demand']) for l in range(nbLoadUnits) for t in range(nbHour)]
+balance_constraint = [m.addConstr(sum(demand_supplied[t, l] for l in range(nbLoadUnits)) - gp.quicksum(production[t, g] for g in range(nbUnits)) == 0, name=f"GenerationBalance_{t+1}") for t in range(nbHour)]
+
+# Solve it!
+m.optimize()
+
+
+################################################################################
+# Result
+################################################################################
+
+production_values = [production[t].X for t in range(nbHour)]
+demand_supplied_values = [demand_supplied[l].X for l in range(nbLoadUnits)]
+
+price = [balance_constraint[t].Pi for t in range(nbHour)]
+profit = [[production[t][g].X*(price[t] - generation_units.units[g]['Cost']) for g in range(nbUnits)] for t in range(nbHour)]
+
+#result
+print(f"Optimal objective value: {m.objVal} $")
+for t in range(nbHour):
+    for g in range(nbUnits):
+        print(f"p_{g+1} for hour {t+1}: production: {production[t][g].X} MW, profit: {profit[t][g]} $")
+print("clearing price:", price)
+#modify the price print to remove the "array()"
